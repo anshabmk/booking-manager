534f72b7b86b90682cacbf60ce24b87376da56bc
anshabmk <anshabmk@live.com>
2020-02-13 17:51:21 +0530

    Adds parsers for time_slot, duration and played_on date

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -9,4 +9,23 @@ module GamesHelper
   def durations
     [['1 hour', 1], ['2 hours', 2]]
   end
+
+  def parse_time_slot(time_slot)
+    hour = time_slot / 100
+    minutes = (time_slot % 100).to_s.ljust(2, '0')
+
+    return "#{hour - 12}:#{minutes} PM" if hour >= 12
+
+    "#{hour}:#{minutes} AM"
+  end
+
+  def parse_date(date)
+    date&.strftime('%d %b %Y')
+  end
+
+  def parse_duration(duration)
+    unit = duration > 1 ? 'hours' : 'hour'
+
+    "#{duration} #{unit}"
+  end
 end
