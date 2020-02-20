7cc86a71bd31f7407495867de493ab8d631fd6dd
anshabmk <anshabmk@live.com>
2020-02-20 17:18:17 +0530

    Adds fullname and games_count methods

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,12 @@
 class Player < ApplicationRecord
   has_and_belongs_to_many :games
   has_many :bill_amounts
+
+  def fullname
+    "#{firstname} #{lastname}"
+  end
+
+  def games_count
+    game_ids.count
+  end
 end
