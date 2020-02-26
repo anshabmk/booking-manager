131ad7b76d8cdd100e93b295db39ddf21d71ee83
anshabmk <anshabmk@live.com>
2020-02-26 17:34:13 +0530

    Splits require_login into require_admin_login and require_player_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_action :require_login
+  before_action :require_admin_login
 
   include SessionsHelper
 
   private
 
-  def require_login
-    return if logged_in?
+  def require_admin_login
+    return if logged_in? && current_user.class.name == 'User'
 
-    flash[:danger] = 'You must be logged in to view the contents of this page'
+    flash.now[:danger] = 'You must be logged in as admin to view the contents of this page.'
+
+    redirect_to(login_url)
+  end
+
+  def require_player_login
+    return if logged_in? && current_user.class.name == 'Player'
+
+    flash.now[:danger] = 'You must be logged in as a player to view the contents of this page.'
 
     redirect_to(login_url)
   end
