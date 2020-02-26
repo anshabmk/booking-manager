453738cf395c4b4a1e4432880d07c909390d7218
anshabmk <anshabmk@live.com>
2020-02-26 18:14:39 +0530

    Selects navbar menu items based on logged in user type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def navbar_menu_items
+    return admin_navbar_menu_items if admin?
+
+    player_navbar_menu_items
+  end
+
+  def player_navbar_menu_items
+    [{ item_name: 'Bill Amounts', url: '/bill_amounts' }]
+  end
+
+  def admin_navbar_menu_items
+    [
+      { item_name: 'Bills', url: '/bills' },
+      { item_name: 'Games', url: '/games' },
+      { item_name: 'Players', url: '/players' }
+    ]
+  end
 end
