f50c223e43c1d0c92fb884e6cbb81ff24df7008d
anshabmk <anshabmk@live.com>
2020-02-26 17:35:45 +0530

    Adds index for logged in players

--- app/controllers/bill_amounts_controller.rb
+++ app/controllers/bill_amounts_controller.rb
@@ -2,8 +2,13 @@
 
 # BillAmounts Controller
 class BillAmountsController < ApplicationController
-  skip_before_action :require_login
+  skip_before_action :require_admin_login
   before_action :set_bill_amount, only: %i[show]
+  before_action :require_player_login, only: %i[index]
+
+  def index
+    @bill_amounts = BillAmount.where(player_id: current_user.id)
+  end
 
   def show; end
 
