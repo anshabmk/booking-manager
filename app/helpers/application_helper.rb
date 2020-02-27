f6528be971c854b8556519c941424d5a624f0b28
anshabmk <anshabmk@live.com>
2020-02-27 18:45:49 +0530

    Adds new navbar menu items for admin and player

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,14 +6,21 @@ module ApplicationHelper
   end
 
   def player_navbar_menu_items
-    [{ item_name: 'Bill Amounts', url: '/bill_amounts' }]
+    [
+      { item_name: 'Bill Amounts', url: '/bill_amounts' },
+      { item_name: 'Profile', url: "/players/#{current_user.id}" }
+    ]
   end
 
   def admin_navbar_menu_items
     [
       { item_name: 'Bills', url: '/bills' },
       { item_name: 'Games', url: '/games' },
-      { item_name: 'Players', url: '/players' }
+      { item_name: 'Players', url: '/players' },
+      {
+        item_name: "Pending Activations(#{Player.inactive_players.count})",
+        url: '/activations/index'
+      }
     ]
   end
 end
