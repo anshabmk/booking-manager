7011e6b5f4db6435b7abd6c4ba342d1fe7287970
anshabmk <anshabmk@live.com>
2020-02-27 18:57:29 +0530

    Adds validations for player attributes

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,19 @@
 class Player < ApplicationRecord
   has_and_belongs_to_many :games
   has_many :bill_amounts
+
+  before_save { self.email_id = email_id.downcase }
+  validates :firstname, presence: true, length: { maximum: 50 }
+  validates :lastname, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email_id, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 
   def fullname
     "#{firstname} #{lastname}"
