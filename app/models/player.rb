e929774506191142f0a1b37223db42e1dca9fc60
anshabmk <anshabmk@live.com>
2020-02-27 19:20:28 +0530

    Modify validations for player attributes

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,13 +4,20 @@ class Player < ApplicationRecord
 
   before_save { self.email_id = email_id.downcase }
   validates :firstname, presence: true, length: { maximum: 50 }
-  validates :lastname, presence: true, length: { maximum: 50 }
+  validates :lastname, length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_MOBILE_NUMBER_REGEX = /\A\d{10}\z/i.freeze
 
-  validates :email_id, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  validates :mobile_number, presence: true,
+                            format: { with: VALID_MOBILE_NUMBER_REGEX },
+                            uniqueness: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :email_id, presence: true,
+                       length: { maximum: 255 },
+                       format: { with: VALID_EMAIL_REGEX },
+                       uniqueness: { case_sensitive: false }
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
