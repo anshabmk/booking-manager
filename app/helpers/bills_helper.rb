75af403d5695181f687b1b1d0a75253bc5626447
anshabmk <anshabmk@live.com>
2020-03-06 15:26:05 +0530

    Adds bill name in index

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,9 +1,9 @@
 module BillsHelper
   def bills_index_header_names
-    ['Date range', 'Fee per game', 'Total amount']
+    ['Name', 'Date range', 'Fee per game', 'Total amount']
   end
 
   def bills_index_header_methods
-    %w[strf_date_range fee_per_game total_amount]
+    %w[name strf_date_range fee_per_game total_amount]
   end
 end
